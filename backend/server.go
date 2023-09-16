d9554f6f34e04f3b50d9cc2b0176a34b10487419
gaahfy <gaahfy@gmail.com>
2023-09-16 20:46:51 +0200

    Fix server being stopped instead of started in GitHub action
    Closes #5

--- backend/server.go
+++ backend/server.go
@@ -8,7 +8,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Langtools updated %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "Langtools updated after bug %s!", r.URL.Path[1:])
 }
 
 func main() {
